7e69524e566afc4d5234dbca4926f210247e0c58
Alex <alexandepp@gmail.com>
2022-05-27 21:03:32 +0300

    utils - add

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@ import './App.css';
 
 function App() {
 
+    const startTrivia = async () => {
+
+    }
+    const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+    }
+    const nextQuestion = () => {
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
 
-      </header>
     </div>
   );
 }

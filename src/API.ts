8d7930be5b624c7eed8315254bbc0c2cdeb492f4
Alex <alexandepp@gmail.com>
2022-06-04 14:33:36 +0300

    API_add_ result

--- src/API.ts
+++ src/API.ts
@@ -5,9 +5,22 @@ export enum Difficulty {
     MEDIUM = 'medium',
     HARD =  'hard'
 }
+export type QuestionsType = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
 
  export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
    const endpoint = (`https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}`)
      const data = await (await fetch(endpoint)).json();
      console.log(data)
+     return data.results.map( (question: QuestionsType) => (
+         {
+
+         }
+     ))
 }

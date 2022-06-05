22b232a145dc1b5f3fbfe27c6be8f00e1e5f1d91
Alex <alexandepp@gmail.com>
2022-06-05 19:14:07 +0300

    App_style_add_

---
+++ src/App.style.ts
@@ -0,0 +1,13 @@
+import styled, {createGlobalStyle} from 'styled-components';
+
+import BGImage from './img/main-background.jpg'
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+
+  }
+`
